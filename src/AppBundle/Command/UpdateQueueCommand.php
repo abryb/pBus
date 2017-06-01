1822dc4ac98bb03585aa318ade3f97c754c46fb2
abryb <andrzej.blazej.rybarkiewicz@gmail.com>
2017-06-01 20:49:21 +0200

    update queue command

--- src/AppBundle/Command/UpdateQueueCommand.php
+++ src/AppBundle/Command/UpdateQueueCommand.php
@@ -5,6 +5,7 @@ namespace AppBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use AppBundle\Entity\UpdateQueue;
 
 class UpdateQueueCommand extends ContainerAwareCommand
 {
@@ -27,6 +28,31 @@ class UpdateQueueCommand extends ContainerAwareCommand
         $output->writeln([
             'Start',
         ]);
+
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $connections = $em->getRepository('AppBundle:Connection')->findTracked();
+
+        $validator = $this->getContainer()->get('validator');
+
+        foreach ($connections as $connection) {
+            $dates = new \DatePeriod(
+                new \DateTime('now'),
+                new \DateInterval('P1D'),
+                $connection->getLastDate()
+            );
+
+            foreach ($dates as $date) {
+                $updateQueue = new UpdateQueue();
+                $updateQueue->setConnection($connection);
+                $updateQueue->setDate($date);
+                $errors = $validator->validate($updateQueue);
+                if (count($errors) == 0) {
+                    $em->persist($updateQueue);
+                }
+            }
+        }
+
+        $em->flush();
 
         $output->writeln('Queue updated');
     }
